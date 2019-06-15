8564151e66ab701ef1ae74279b5acbe3cd7ed771
Jordan Roberts <jordanroberts@talk21.com>
2019-06-15 15:43:32 +0100

    Added UITextFieldDelegate functions to respond to the user interactions

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    //The Text Field Delegate - A set of optional methods that you use to manage the editing and validation of text in a text field object.
+    //The Text Field Delegate - A set of optional methods that you use to manage the editing and validation of text in a text field object. This means you can implement the protocol’s methods to handle text input, and you can assign instances of the ViewController class as the delegate of the text field.
     
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
@@ -18,7 +18,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Handle the text field user's input through delegate callbaccks
+        nameTextField.delegate = self
+        // self here refers to ViewController class, as it's referenced inside the scope of the ViewController class definition
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
     
     // MARK: Actions
