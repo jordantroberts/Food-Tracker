cc68d7a0c8d86a99c3c178fec4f0a6b1df8118c7
Jordan Roberts <jordanroberts@talk21.com>
2019-06-15 15:34:14 +0100

    Added UITextFieldDelegate to communicate with text field

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    // MARK: Properties
+class ViewController: UIViewController, UITextFieldDelegate {
     
+    //The Text Field Delegate - A set of optional methods that you use to manage the editing and validation of text in a text field object.
+    
+    // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
@@ -20,8 +22,10 @@ class ViewController: UIViewController {
     }
     
     // MARK: Actions
-    
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+        mealNameLabel.text = "Default text"
+        
     }
     
 }
